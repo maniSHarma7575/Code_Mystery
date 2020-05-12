003dfc87660697da02d83e0516356de1e6aaf68f
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-12 13:24:26 +0530

    Kadane's Algorithm

---
+++ 017_kandane_algorithm.cpp
@@ -0,0 +1,19 @@
+int maxSubarraySum(int arr[], int n){
+    
+    int max_ending_here=0;
+    int max_so_far=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        max_ending_here+=arr[i];
+        if(max_so_far<max_ending_here)
+        {
+            max_so_far=max_ending_here;
+        }
+        if(max_ending_here<0)
+        {
+            max_ending_here=0;
+        }
+    }
+    return max_so_far;
+    
+}
