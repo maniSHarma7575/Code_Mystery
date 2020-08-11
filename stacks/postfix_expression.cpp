adcbdfa277eacf0c47a6ee042b4d789f76dd37f3
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-08-11 22:15:19 +0530

    Updated

--- stacks/postfix_expression.cpp
+++ stacks/postfix_expression.cpp
@@ -1,15 +1,36 @@
-void reverse(char *str, int len)
+
+int calculate(int a,int b,char op){
+    switch(op){
+        case '*':
+        return a*b;
+        case '-':
+        return a-b;
+        case '+':
+        return a+b;
+        case '/':
+        return a/b;
+    }
+}
+bool isNumber(char a){
+    if((int)a>=48 && (int)a<=57)return true;
+    return false;
+}
+int evaluatePostfix(string &str)
 {
-stack<char> s;
-for(int i=0;i<len;i++){
-    s.push(str[i]);
-}
-
-int i=0;
-while(!s.empty()){
-    str[i]=s.top();
-    s.pop();
-    i++;
-}
-
+    int n=str.length();
+    stack<int> s;
+    for(int i=0;i<n;i++){
+        if(isNumber(str[i])){
+            s.push(str[i]-48);
+        }
+        else{
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            int res=calculate(b,a,str[i]);
+            s.push(res);
+        }
+    }
+    return s.top();
 }
