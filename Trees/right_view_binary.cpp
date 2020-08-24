717741b58883568dd8f69b4f1f4a89abea8b2f8c
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-08-24 21:59:16 +0530

    Resolve and added Left view

--- Trees/right_view_binary.cpp
+++ Trees/right_view_binary.cpp
@@ -1,57 +1,31 @@
 void rightView(Node *root)
 {
-   
-      // Declare a queue for Level order Traversal
       queue<Node*> q;
   
     if (root == NULL) 
         return; 
 
-    // Push root 
     q.push(root); 
 
-    // Delimiter 
     q.push(NULL); 
 
     while (!q.empty()) { 
         Node* temp = q.front(); 
         if (temp) { 
-
-            // Prints first node 
-            // of each level 
-
-            // Push children of all nodes at 
-            // current level 
-            while (q.front() != NULL) { 
-
-                // If left child is present 
-                // push into queue 
+            while (q.front() != NULL) {
                 if (temp->left) 
                     q.push(temp->left); 
-
-                // If right child is present 
-                // push into queue 
                 if (temp->right) 
                     q.push(temp->right); 
-
-                // Pop the current node 
                 q.pop(); 
                 if(q.front()==NULL){
                 	cout<<temp->data<<" ";
                 }
                 temp = q.front(); 
                
-            } 
-            
+            }            
 			 q.push(NULL); 
-
-            // Push delimiter 
-            // for the next level 
-           
         } 
-
-        // Pop the delimiter of 
-        // the previous level 
         q.pop(); 
     } 
 }
